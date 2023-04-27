948d6a1592196e67d511209175e7f8ca53de5d44
Жамекешев Дастан <edaz@technodom.kz>
2023-04-27 14:22:45 +0600

    feat: добавил в CustomSelect поисковой инпут для удобства пользователя

--- src/components/UI/CustomSelect/CustomSelect.jsx
+++ src/components/UI/CustomSelect/CustomSelect.jsx
@@ -3,6 +3,8 @@ import styles from './CustomSelect.module.css';
 import { v4 as uuidv4 } from 'uuid';
 
 const CustomSelect = ({ icon, placeholder, label, isRequired, name, data, value, onClick, variant }) => {
+    const [query, setQuery] = useState('');
+    const [filteredResults, setFilteredResults] = useState(data);
     const [isOpen, setIsOpen] = useState(false);
     const selectRef = useRef(null);
 
@@ -17,6 +19,19 @@ const CustomSelect = ({ icon, placeholder, label, isRequired, name, data, value,
             }
         }
     }
+
+    const handleSearch = ({ target }) => setQuery(target.value);
+
+    useEffect(() => {
+        const filteredData = data?.length > 0 && data.filter(item => item.toLowerCase().includes(query.toLowerCase()));
+        setFilteredResults(filteredData);
+    }, [data, query])
+
+    useEffect(() => {
+        if (!isOpen) {
+            setQuery('');
+        }
+    }, [isOpen])
 
     useEffect(() => {
         const handleClickOutside = ({ target }) => {
@@ -49,8 +64,11 @@ const CustomSelect = ({ icon, placeholder, label, isRequired, name, data, value,
                          src="https://www.technodom.kz/under/briefer/chevron-black.svg" alt=""/>
                 </button>
                 <div className={styles.customSelectContent}>
+                    {data.length > 8 &&
+                        <input value={query} className={styles.searchInput} type="search" onChange={handleSearch}
+                               placeholder="Давайте поищем..."/>}
                     <ul className={styles.customSelectOptionsList}>
-                        {data.length > 0 && data.map(item => {
+                        {filteredResults.length > 0 && filteredResults.map(item => {
                             return <li tabIndex={0} key={uuidv4()} onClick={handleClick(item)}>{item}</li>
                         })}
                     </ul>
