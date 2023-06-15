cf25de192516b742cb0d2f4bfa48b80470f4a934
Dastan <edaz@technodom.kz>
2023-06-15 14:36:08 +0600

    fix: поправил ссылку на бриф и отображение времени на выполнение

--- src/components/ArchiveThumb/ArchiveThumb.jsx
+++ src/components/ArchiveThumb/ArchiveThumb.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './ArchiveThumb.module.css';
+import { Link } from 'react-router-dom';
+import { declinate } from '../../utils/declinate';
 
 const ArchiveThumb = ({ data }) => {
-    const { title, uuid, date_start, date_end } = data;
+    const { title, uuid, date_start, date_end, time_to_create } = data;
     const start = new Date(date_start).toLocaleDateString();
     const end = new Date(date_end).toLocaleDateString();
-
+    const timeToDo = time_to_create ? `${time_to_create} ${declinate(time_to_create, 'hours')}` : 'неизвестно';
+    
     return (
         <li>
             <article className={styles.archbrief}>
@@ -15,10 +18,10 @@ const ArchiveThumb = ({ data }) => {
                         <div className={styles.info}>
                             <span>Номер: <strong>{uuid.slice(0, 8)}</strong></span>
                             <span>Продолжительность: <strong>{start} - {end}</strong></span>
-                            <span>Время на выполнение: <strong>16 часов</strong></span>
+                            <span>Время на выполнение: <strong>{timeToDo}</strong></span>
                         </div>
                     </div>
-                    <a className={styles.link} href="https://www.google.com">Перейти</a>
+                    <Link className={styles.link} to={`/briefs/${uuid}`}>Перейти</Link>
                 </div>
             </article>
         </li>
